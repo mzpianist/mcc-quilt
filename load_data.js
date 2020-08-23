05775e5d16504f5322328aa6b5db253b05f3f1e8
elaine <epham@mit.edu>
2020-08-23 18:34:12 -0500

    starting popup

--- load_data.js
+++ load_data.js
@@ -1,11 +1,12 @@
-//import gapi from "https://apis.google.com/js/api.js"
-
 // Client ID and API key from the Developer Console
-var CLIENT_ID = '468244446404-9ve3tahnk9ihbpomp1k7hrqru2krmqpe.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyDwftw9IiwqEkpf5Bsx5gT73X1Ws-GKcyU';
+var CLIENT_ID =
+  "468244446404-9ve3tahnk9ihbpomp1k7hrqru2krmqpe.apps.googleusercontent.com";
+var API_KEY = "AIzaSyDwftw9IiwqEkpf5Bsx5gT73X1Ws-GKcyU";
 
 // Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+var DISCOVERY_DOCS = [
+  "https://sheets.googleapis.com/$discovery/rest?version=v4",
+];
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
@@ -15,7 +16,7 @@ var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
  *  On load, called to load the auth2 library and API client library.
  */
 function handleClientLoad() {
-  gapi.load('client:auth2', initClient);
+  gapi.load("client:auth2", initClient);
 }
 
 /**
@@ -23,16 +24,21 @@ function handleClientLoad() {
  *  listeners.
  */
 function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-    scope: SCOPES
-  }).then(function () {
-    listImages()
-  }, function (error) {
-    console.log(JSON.stringify(error, null, 2));
-  });
+  gapi.client
+    .init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES,
+    })
+    .then(
+      function () {
+        listImages();
+      },
+      function (error) {
+        console.log(JSON.stringify(error, null, 2));
+      }
+    );
 }
 
 /**
@@ -40,44 +46,52 @@ function initClient() {
  * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  */
 function listImages() {
-  gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: '1BPwDzWroFk-hSNZ4qFr5FFwItIMb6uvkoFJYBbQoe5w',
-    range: 'B:J',
-  }).then(function (response) {
-    var range = response.result;
-    var response_list = []
-    if (range.values.length > 0) {
-      //response_dict = {'Type': [], 'Name': [], 'Email':[], 'Year':[], 'Image':[], 'Bio':[], 'Comments':[], 'Socials':[], 'Accounts':[]}
-      for (i = 1; i < range.values.length; i++) {
-        var row = range.values[i];
-        response_list.push(row)
+  gapi.client.sheets.spreadsheets.values
+    .get({
+      spreadsheetId: "1BPwDzWroFk-hSNZ4qFr5FFwItIMb6uvkoFJYBbQoe5w",
+      range: "B:J",
+    })
+    .then(
+      function (response) {
+        var range = response.result;
+        var response_list = [];
+        if (range.values.length > 0) {
+          //response_dict = {'Type': [], 'Name': [], 'Email':[], 'Year':[], 'Image':[], 'Bio':[], 'Comments':[], 'Socials':[], 'Accounts':[]}
+          for (i = 1; i < range.values.length; i++) {
+            var row = range.values[i];
+            response_list.push(row);
+          }
+          shuffle(response_list);
+          loadImages(response_list);
+          handleClick(response_list);
+        } else {
+          console.log("No data found.");
+        }
+      },
+      function (response) {
+        console.log("Error: " + response.result.error.message);
       }
-      shuffle(response_list)
-      loadImages(response_list)
-    } else {
-      console.log('No data found.');
-    }
-  }, function (response) {
-    console.log('Error: ' + response.result.error.message);
-  });
+    );
 }
 
 function loadImages(response_list) {
-  const root = document.getElementById('root')
-  for (i = 0; i < response_list.length; i++){
-    const column = root.children[i%4]
-    const image = document.createElement("img")
-    image.setAttribute("src",response_list[i][4].replace("open","thumbnail"))
-    column.appendChild(image)
+  const root = document.getElementById("root");
+  for (i = 0; i < response_list.length; i++) {
+    const column = root.children[i % 4];
+    const image = document.createElement("img");
+    image.setAttribute("src", response_list[i][4].replace("open", "thumbnail"));
+    image.setAttribute("key", i);
+    column.appendChild(image);
   }
 }
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -89,4 +103,13 @@ function shuffle(array) {
   }
 
   return array;
+}
+
+function handleClick(array){
+  document.getElementById('root').onclick = function(event){
+    const popup = document.getElementById('popup');
+    popup.style.visibility = 'visible';
+    const index = event.target.getAttribute('key');
+    popup.innerText = array[index];
+  }
 }
